ad1021d14f2ddad4219e1b40bd3c964ec6ebc7c1
chez1s <toptrainghieml@gmail.com>
2025-08-02 21:34:31 +0700

    Add DemoWatchEffects.vue and axios dependency
    Added a new Vue component DemoWatchEffects.vue with a button and count state. Also added axios to project dependencies in package.json.

---
+++ src/DemoWatchEffects.vue
@@ -0,0 +1,13 @@
+<template>
+
+<button class="btn btn-primary" @click="">Plus 1</button>
+
+</template>
+
+<script setup>
+
+import { ref, watchEffect } from 'vue';
+
+const count = ref(0);
+
+</script>
